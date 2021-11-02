289f9c48b80b1e647a5c84a8203ba04087e2cfb1
Jeff Pride <jpride@gmail.com>
2021-11-02 18:57:23 -0500

    Add project files.

---
+++ TSI.FourSeries.CommandQueue/CommandQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace TSI.FourSeries.CommandQueue
+{
+    public class CommandQueue
+    {
+        public Queue<string> CommandQueueObj = new Queue<string>();
+        
+
+        public CommandQueue()
+        { 
+            //empty instatiator        
+        }
+
+
+        public void AddCommand(string cmd)
+        {
+            CommandQueueObj.Enqueue(cmd);
+        }
+
+        public void ProcessQueue()
+        {
+            if (CommandQueueObj.Count > 0)
+            {
+                ProcessQueueEventArgs args = new ProcessQueueEventArgs
+                {
+                    cmd = CommandQueueObj.Dequeue()
+                };
+
+                if (!ProcessQueueEventCall.Equals(null))
+                {
+                    ProcessQueueEventCall(this, args);
+                }
+            }
+        }
+
+        public event EventHandler<ProcessQueueEventArgs> ProcessQueueEventCall;
+    }
+}
